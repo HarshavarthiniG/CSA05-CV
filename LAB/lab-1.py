7b78630e16a1eddd8a152eafbc988b77801c9165
HarshavarthiniG <192421378.simats@saveetha.com>
2026-07-29 10:22:25 +0530

    lab-1.py

--- LAB/lab-1.py
+++ LAB/lab-1.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread(r"C:\Users\Akhila\OneDrive\Desktop\ITA0510-LAB\Picture1.jpg")
+img = cv2.imread(r"C:\Users\Harsha\OneDrive\Desktop\ITA0510-LAB\Picture1.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Grayscale Image", gray_img)
 cv2.imwrite("gray_image.jpg", gray_img)
